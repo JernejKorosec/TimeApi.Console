66af8d24f50d0d503ba708404dee1544bd8e7bd9
Jernej Korosec <jernejk.alfa@gmail.com>
2022-02-01 00:30:43 +0100

    Ekola, svoja angular komponenta z lepo tabelco + ostale komponente, ki se rabjo narest...

--- TimeApi.Angular.NoWebApiIntegration/TimeApi.Angular/angular/src/app/app.module.ts
+++ TimeApi.Angular.NoWebApiIntegration/TimeApi.Angular/angular/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatListModule } from '@angular/material/list';
 import { MatTableModule } from '@angular/material/table';
+import { SettingsComponent } from './settings/settings.component';
+import { UsersComponent } from './users/users.component';
+import { PresenceComponent } from './presence/presence.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent,
+    UsersComponent,
+    PresenceComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,

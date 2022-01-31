3d3ad7916666a79098a6b8cc02cea54bfbd36214
Jernej Korosec <jernejk.alfa@gmail.com>
2022-02-01 00:23:48 +0100

    test mal angular tabelc...

---
+++ TimeApi.Angular.NoWebApiIntegration/TimeApi.Angular/angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatTableModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
